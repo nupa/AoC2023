2a94d719822a8a0d531772bef580a27339e50103
Panu Åkerman <panu.akerman@evident.fi>
2023-12-10 11:06:55 +0200

    simplify hand creation

--- src/main/kotlin/nupa/aoc23/days/day7/Day7.kt
+++ src/main/kotlin/nupa/aoc23/days/day7/Day7.kt
@@ -16,20 +16,14 @@ fun day7Part1() {
         }
         val cards = cardsWithAmount.toList().sortedBy { it.second }.reversed()
         val hand = when (cards[0].second) {
-            5 -> Hand.FiveOfKind(cards[0].first, handAndBid.first)
-            4 -> Hand.FourOfKind(cards[0].first, cards[1].first, handAndBid.first)
+            5 -> Hand.FiveOfKind(handAndBid.first)
+            4 -> Hand.FourOfKind(handAndBid.first)
             3 -> if (cards[1].second == 2)
-                Hand.FullHouse(cards[0].first, cards[1].first, handAndBid.first)
+                Hand.FullHouse(handAndBid.first)
             else
-                Hand.ThreeOfKind(cards[0].first, handAndBid.first)
-            2 -> if (cards[1].second == 2) {
-                val card1 = cards[0].first
-                val card2 = cards[1].first
-                if (CardComparator.compare(card1, card2) < 0) Hand.TwoPairs(card1, card2, cards[2].first, handAndBid.first) else Hand.TwoPairs(card2, card1, cards[2].first, handAndBid.first)
-            } else {
-                Hand.Pair(cards[0].first, handAndBid.first)
-            }
-            else -> Hand.HighCard(cards.map { it.first }.sortedWith(CardComparator).first(), handAndBid.first)
+                Hand.ThreeOfKind(handAndBid.first)
+            2 -> if (cards[1].second == 2) Hand.TwoPairs(handAndBid.first) else Hand.Pair(handAndBid.first)
+            else -> Hand.HighCard(handAndBid.first)
         }
         Game(hand, handAndBid.second.toInt())
     }
@@ -61,55 +55,54 @@ object SimpleGameComparator: Comparator<Game> {
 class Game(val hand: Hand, val bid: Int)
 
 sealed class Hand(val allCards: String) {
-    val sortedAllCards = allCards.toList().sortedWith(CardComparator)
     abstract val rank: Int
 
-    class FiveOfKind(val card: Char, allCards: String): Hand(allCards) {
+    class FiveOfKind(allCards: String): Hand(allCards) {
         override val rank: Int = 1
         override fun toString(): String =
-            "5 of a kind with $card ($sortedAllCards)"
+            "5 of a kind with $allCards"
     }
 
-    class FourOfKind(val card: Char, val single: Char, allCards: String): Hand(allCards) {
+    class FourOfKind(allCards: String): Hand(allCards) {
         override val rank: Int = 2
 
         override fun toString(): String =
-            "4 of a kind with $card ($sortedAllCards)"
+            "4 of a kind with $allCards"
     }
 
-    class FullHouse(val threeOfKindCard: Char, val pairCard: Char, allCards: String): Hand(allCards) {
+    class FullHouse(allCards: String): Hand(allCards) {
         override val rank: Int = 3
 
         override fun toString(): String =
-            "full house with 3 $threeOfKindCard and 2 $pairCard ($sortedAllCards)"
+            "full house with 3 $allCards"
     }
 
-    class ThreeOfKind(val card: Char, allCards: String): Hand(allCards) {
+    class ThreeOfKind(allCards: String): Hand(allCards) {
         override val rank: Int = 4
 
         override fun toString(): String =
-            "3 of a kind with $card ($sortedAllCards)"
+            "3 of a kind with $allCards"
     }
 
-    class TwoPairs(val highPairCard: Char, val lowPairCard: Char, val single: Char, allCards: String): Hand(allCards) {
+    class TwoPairs(allCards: String): Hand(allCards) {
         override val rank: Int = 5
 
         override fun toString(): String =
-            "two pairs with $highPairCard (high) and $lowPairCard (low) ($sortedAllCards)"
+            "two pairs with $allCards"
     }
 
-    class Pair(val card: Char, allCards: String): Hand(allCards) {
+    class Pair(allCards: String): Hand(allCards) {
         override val rank: Int = 6
 
         override fun toString(): String =
-            "pair with $card ($sortedAllCards)"
+            "pair with $allCards"
     }
 
-    class HighCard(val card: Char, allCards: String): Hand(allCards) {
+    class HighCard(allCards: String): Hand(allCards) {
         override val rank: Int = 7
 
         override fun toString(): String =
-            "high card with $card ($sortedAllCards)"
+            "high card with $allCards"
     }
 }
 
