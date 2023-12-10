442e025e52786c8b001cdbb0611adca996d0c89d
Panu Åkerman <panu.akerman@evident.fi>
2023-12-10 11:00:27 +0200

    remove real hand comparison

--- src/main/kotlin/nupa/aoc23/days/day7/Day7.kt
+++ src/main/kotlin/nupa/aoc23/days/day7/Day7.kt
@@ -2,8 +2,7 @@ package nupa.aoc23.days.day7
 
 import nupa.aoc23.input.lines
 import nupa.aoc23.input.splitToPair
-import kotlin.jvm.internal.Intrinsics.Kotlin
-import kotlin.reflect.typeOf
+
 
 fun day7Part1() {
     val lines = lines("day7/input.txt")
@@ -32,7 +31,6 @@ fun day7Part1() {
             }
             else -> Hand.HighCard(cards.map { it.first }.sortedWith(CardComparator).first(), handAndBid.first)
         }
-        //println(hand)
         Game(hand, handAndBid.second.toInt())
     }
     println(games.sortedWith(SimpleGameComparator).reversed().withIndex().fold(0L) { acc, gameWithIndex -> acc + (gameWithIndex.index + 1) * gameWithIndex.value.bid })
@@ -55,32 +53,9 @@ object SimpleHandComparator: Comparator<Hand> {
     }
 }
 
-object HandComparator: Comparator<Hand> {
-    override fun compare(o1: Hand, o2: Hand): Int {
-        return if (o1::class == o2::class) {
-            when (o1) {
-                is Hand.FiveOfKind-> o1.compare(o2 as Hand.FiveOfKind)
-                is Hand.FourOfKind -> o1.compare(o2 as Hand.FourOfKind)
-                is Hand.FullHouse -> o1.compare(o2 as Hand.FullHouse)
-                is Hand.ThreeOfKind -> o1.compare(o2 as Hand.ThreeOfKind)
-                is Hand.Pair -> o1.compare(o2 as Hand.Pair)
-                is Hand.TwoPairs -> o1.compare(o2 as Hand.TwoPairs)
-                is Hand.HighCard -> o1.compare(o2 as Hand.HighCard)
-            }
-        } else {
-            o1.rank - o2.rank
-        }
-    }
-}
-
 object SimpleGameComparator: Comparator<Game> {
     override fun compare(o1: Game, o2: Game): Int =
         SimpleHandComparator.compare(o1.hand, o2.hand)
-}
-
-object GameComparator: Comparator<Game> {
-    override fun compare(o1: Game, o2: Game): Int =
-        HandComparator.compare(o1.hand, o2.hand)
 }
 
 class Game(val hand: Hand, val bid: Int)
@@ -93,9 +68,6 @@ sealed class Hand(val allCards: String) {
         override val rank: Int = 1
         override fun toString(): String =
             "5 of a kind with $card ($sortedAllCards)"
-
-        fun compare(other: FiveOfKind): Int =
-            CardComparator.compare(card, other.card)
     }
 
     class FourOfKind(val card: Char, val single: Char, allCards: String): Hand(allCards) {
@@ -103,11 +75,6 @@ sealed class Hand(val allCards: String) {
 
         override fun toString(): String =
             "4 of a kind with $card ($sortedAllCards)"
-
-        fun compare(other: FourOfKind): Int {
-            val fourComparison = CardComparator.compare(card, other.card)
-            return if (fourComparison == 0) CardComparator.compare(single, other.single) else fourComparison
-        }
     }
 
     class FullHouse(val threeOfKindCard: Char, val pairCard: Char, allCards: String): Hand(allCards) {
@@ -115,11 +82,6 @@ sealed class Hand(val allCards: String) {
 
         override fun toString(): String =
             "full house with 3 $threeOfKindCard and 2 $pairCard ($sortedAllCards)"
-
-        fun compare(other: FullHouse): Int {
-            val firstCompare = CardComparator.compare(threeOfKindCard, other.threeOfKindCard)
-            return if (firstCompare == 0) CardComparator.compare(pairCard, other.pairCard) else firstCompare
-        }
     }
 
     class ThreeOfKind(val card: Char, allCards: String): Hand(allCards) {
@@ -127,15 +89,6 @@ sealed class Hand(val allCards: String) {
 
         override fun toString(): String =
             "3 of a kind with $card ($sortedAllCards)"
-
-        fun compare(other: ThreeOfKind): Int {
-            val pairCompare = CardComparator.compare(card, other.card)
-            return if (pairCompare == 0) {
-                sortedAllCards.compareRest(card, other.sortedAllCards)
-            } else {
-                pairCompare
-            }
-        }
     }
 
     class TwoPairs(val highPairCard: Char, val lowPairCard: Char, val single: Char, allCards: String): Hand(allCards) {
@@ -143,20 +96,6 @@ sealed class Hand(val allCards: String) {
 
         override fun toString(): String =
             "two pairs with $highPairCard (high) and $lowPairCard (low) ($sortedAllCards)"
-
-        fun compare(other: TwoPairs): Int {
-            val highCompare = CardComparator.compare(highPairCard, other.highPairCard)
-            return if (highCompare == 0) {
-                val lowCompare = CardComparator.compare(lowPairCard, other.lowPairCard)
-                if (lowCompare == 0) {
-                    CardComparator.compare(single, other.single)
-                } else {
-                    lowCompare
-                }
-            } else {
-                highCompare
-            }
-        }
     }
 
     class Pair(val card: Char, allCards: String): Hand(allCards) {
@@ -164,15 +103,6 @@ sealed class Hand(val allCards: String) {
 
         override fun toString(): String =
             "pair with $card ($sortedAllCards)"
-
-        fun compare(other: Pair): Int {
-            val cardCompare = CardComparator.compare(card, other.card)
-            return if (cardCompare == 0) {
-                sortedAllCards.compareRest(card, other.sortedAllCards)
-            } else {
-                cardCompare
-            }
-        }
     }
 
     class HighCard(val card: Char, allCards: String): Hand(allCards) {
@@ -180,14 +110,8 @@ sealed class Hand(val allCards: String) {
 
         override fun toString(): String =
             "high card with $card ($sortedAllCards)"
-
-        fun compare(other: HighCard): Int =
-            sortedAllCards.zip(other.sortedAllCards).firstNotNullOfOrNull { (o1, o2) -> CardComparator.compare(o1, o2).takeIf { it != 0 } } ?: 0
     }
 }
-
-fun List<Char>.compareRest(comparedCard: Char, otherCards: List<Char>): Int =
-    filter { it != comparedCard }.zip(otherCards.filter { it != comparedCard }).compareUntilDifferent()
 
 fun List<Pair<Char, Char>>.compareUntilDifferent(): Int =
     firstNotNullOfOrNull { (o1, o2) -> CardComparator.compare(o1, o2).takeIf { it != 0 } } ?: 0
